cb8c4774866aeb9675a30c2e85868ca68a5d7ae9
Gabriel Felipe <developergb@hotmail.com>
2024-03-28 15:30:17 -0300

    feat: aula09

---
+++ aula-09.js
@@ -0,0 +1,17 @@
+function soma(item1, item2) {
+    return item1 + item2;
+}
+console.log('SOMA: ', soma(5, 6));
+console.log('SOMA: ', soma(1000, 16));
+
+function diminuir(item1, item2) {
+    return item1 - item2
+}
+console.log('SUB: ', diminuir(5, 6));
+console.log('SUB: ', diminuir(509, 63));
+
+function multi(item1, item2) {
+    return item1 * item2
+}
+console.log('MULTI: ', multi(5, 6));
+console.log('MULTI: ', multi(7, 5));
