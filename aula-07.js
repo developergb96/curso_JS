bf02acf21ac4d3c994cab198e0bf6a82450f1873
Gabriel Felipe <developergb@hotmail.com>
2024-03-28 15:10:26 -0300

    feat: aula07

---
+++ aula-07.js
@@ -0,0 +1,14 @@
+const idadeJulio = 18;
+const idadeJoao = 14;
+const idadeJose = 53;
+
+const idades = [18, 14, 53, 23, 51, 'gb'];
+
+console.log('Resultado:', idades[0]);
+console.log('Resultado:', idades[1]);
+console.log('Resultado:', idades[2]);
+console.log('Quantos objetos tem no array?', idades.length);
+
+for(let i = 0;  i < idades.length; i++) {
+    console.log('Resultado:', idades[i]);
+}
