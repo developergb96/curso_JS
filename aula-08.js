5fc03688ea8ef6cd68dfe5d6af2fdb2b68332ebb
Gabriel Felipe <developergb@hotmail.com>
2024-03-28 15:21:29 -0300

    feat: aula08

---
+++ aula-08.js
@@ -0,0 +1,11 @@
+const salario1 = 103.4;
+const salario2 = 103.873636163163;
+const salario3 = 103;
+const salarioNumero = Number(salario3);
+
+console.log('Salario:', salario1.toFixed(2));
+console.log('Salario:', salario2.toFixed(2));
+
+console.log('Salario:', salario3);
+console.log('Salario numero:' , salarioNumero)
+console.log('Salario numero:', salarioNumero.toFixed(2));
